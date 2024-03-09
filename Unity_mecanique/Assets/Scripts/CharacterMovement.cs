6e513331453d65500ff5fad911efbfbef8b359bc
TRISTAN <debeaunetristan@gmail.com>
2024-03-09 22:01:31 +0100

    first movement version

--- Unity_mecanique/Assets/Scripts/CharacterMovement.cs
+++ Unity_mecanique/Assets/Scripts/CharacterMovement.cs
@@ -1,32 +1,95 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
-    private bool isGrounded = false;
+    public float jumpForce = 10f;
+    public float mouvementSpeed = 5f;
 
+    [Range(0, 1)]
+    public float airControl = 0.5f;
+
+    [SerializeField]
     private CapsuleCollider capsuleCollider;
+
+    private Rigidbody rb;
+
+    private PlayerInput playerInput;
+
+    private Vector2 movementInput = Vector2.zero;
+
+    [SerializeField]
+    private LayerMask groundMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        capsuleCollider = GetComponent<CapsuleCollider>();
+        // capsuleCollider = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() { }
 
-    private void OnCollisionEnter(Collision other)
+    bool IsGrounded()
     {
-        foreach (var contactPoint in other.contacts)
+        return Physics
+                .OverlapSphere(
+                    transform.position + Vector3.down * capsuleCollider.height / 2,
+                    0.3f,
+                    groundMask
+                )
+                .Length > 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
         {
-            Debug.Log(contactPoint.normal);
-            Debug.Log(Vector3.Dot(contactPoint.normal, Vector3.up));
-            isGrounded = Vector3.Dot(contactPoint.normal, Vector3.up) > 0;
+            ApplyMovement(movementInput);
         }
     }
 
-    private void OnCollisionExit(Collision other)
+    private void OnDrawGizmos()
     {
-        isGrounded = false;
+        Gizmos.DrawSphere(transform.position + Vector3.down * capsuleCollider.height / 2, 0.3f);
+    }
+
+    private void ApplyJumpForce()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ApplyMovement(Vector2 Inputs)
+    {
+        Vector3 InputsVec3 = transform.forward * Inputs.y + transform.right * Inputs.x;
+        // Vector3 newPosition = transform.position + InputsVec3 * Time.deltaTime * mouvementSpeed;
+        // rb.Move(newPosition, transform.rotation);
+        rb.AddForce(InputsVec3 * mouvementSpeed * Time.deltaTime, ForceMode.Impulse);
+    }
+
+    // event from the input action
+    private void OnJump()
+    {
+        if (!IsGrounded())
+            return;
+        // Debug.Log("jump");
+        ApplyJumpForce();
+    }
+
+    private void OnMovement(InputValue inputValue)
+    {
+        Debug.Log("ça bouuuge");
+        Debug.Log(inputValue.Get<Vector2>());
+
+        // ApplyMovement(inputValue.Get<Vector2>());
+        movementInput = inputValue.Get<Vector2>();
+    }
+
+    private void OnLook(InputValue inputValue)
+    {
+        Debug.Log(inputValue.Get<Vector2>());
     }
 }
