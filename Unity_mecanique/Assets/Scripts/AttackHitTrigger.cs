f6b784f74c72884c73e2367870c1717f25f436af
TRISTAN <debeaunetristan@gmail.com>
2024-04-26 02:37:58 +0200

    fix ennemi lazer rotation attack (not deal dammages again)

--- Unity_mecanique/Assets/Scripts/AttackHitTrigger.cs
+++ Unity_mecanique/Assets/Scripts/AttackHitTrigger.cs
@@ -7,11 +7,11 @@ public class AttackHitTrigger : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField]
-    private Attack attack;
+    private IAttack attack;
 
     void Start()
     {
-        attack = GetComponentInParent<Attack>();
+        attack = GetComponentInParent<IAttack>();
     }
 
     // Update is called once per frame
@@ -19,14 +19,16 @@ public class AttackHitTrigger : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        IDammagable dammagable = null;
+        // IDammagable dammagable = null;
+        Debug.Log(other.transform.name + "touchééééééééé");
 
         if (other.transform.gameObject.TryGetComponent(out IDammagable Outdammagable))
         {
-            dammagable = Outdammagable;
-            dammagable.TakeDammage(attack.Dammages);
+            if (Outdammagable == null)
+                Debug.Log("out dammage est null dans le lazer shot");
+            // Outdammagable = Outdammagable;
+            Outdammagable.TakeDammage(attack.Dammages);
             Debug.Log(other.transform.name + "touchééééééééé AVEC DES DEGATS ????");
         }
-        Debug.Log(other.transform.name + "touchééééééééé");
     }
 }
